21635aed67aaea88b4bee7bcb31cfedfb03bb220
Cheetah <wuxin31911@163.com>
2025-03-15 15:23:06 +0800

    init

---
+++ main.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import numpy as np
+import seaborn as sn
+from stockstats import StockDataFrame
+import datetime
+import matplotlib.pyplot as plt
+
+
+filename = "SHFE.NI.CSV"
+df = pd.read_csv(filename)
+stock_data = StockDataFrame.retype(df)
+print(df.columns)
+df.describe()
+df['y'] = df['datetime'].map(lambda x: x[:4])
+pass
