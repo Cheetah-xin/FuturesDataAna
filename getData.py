2a20e7cac6d3de4dd5b3263dfb004f17ca1008b4
Cheetah <cheetah@CheetahdePro13.local>
2025-03-26 17:38:14 +0800

    daily not passed

--- getData.py
+++ getData.py
@@ -1,10 +1,8 @@
 import pandas as pd
 import numpy as np
-import tushare as ts
-import pandas_datareader as pdr
-import yfinance as yf
 import akshare as ak
 from datetime import datetime
+import re
     
 
 """get data from pandas_datareader
@@ -24,12 +22,33 @@ def getdata2(future_code='AAPL',
 def get_futures_minute_data(symbol):
     futures_zh_minute_sina_df = ak.futures_zh_minute_sina(symbol=symbol, period="1")
     return futures_zh_minute_sina_df
+def get_futures_daily_data(symbol, start,end):
+    futures_daily_df = ak.futures_zh_daily_sina(symbol=symbol)
+    return futures_daily_df
 
 if __name__=="__main__":
-    symbol = 'RM2505'  # 期货合约代码
-    data = get_futures_minute_data(symbol)
-    filename = symbol+".csv"
-    data.to_csv(filename)
-    
-    
+    symbol_set={"RM2505","OI2505","m2505","ni2505"}
+    # minite
+    for symbol in symbol_set:
+        try:
+            data = get_futures_minute_data(symbol)
+            start=data['datetime'].min()
+            start_str = re.sub(r'[ :-]', '',start)
+            end = data['datetime'].max()
+            end_str = re.sub(r'[ :-]', '',end)
+            filename_minute = "./download_data/{symbol}_min_{start}_{end}.csv".format(symbol=symbol,start=start_str,end=end_str)
+            data.to_csv(filename_minute,index=False)
+        except Exception as e:
+            print (e)
+            continue
+
+    # daily
+    for symbol in symbol_set:
+        try:
+            data = get_futures_daily_data(symbol=symbol)
+            filename_daily = "./download_data/{symbol}_daily.csv".format(symbol=symbol)
+            data.to_csv(filename_daily)
+        except Exception as e:
+            print(e)
+            continue
 
