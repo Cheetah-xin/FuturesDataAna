9c08d6985aaa2bdb4ff8d4f860886fcb1ead5ad5
Cheetah <cheetah@CheetahdePro13.local>
2025-04-25 23:18:03 +0800

    backtrader

--- getData.py
+++ getData.py
@@ -28,7 +28,8 @@ def get_futures_daily_data(symbol):
 
 if __name__=="__main__":
     symbol_set={"RM2505","OI2505","m2505","ni2505"}
-    symbol_set={"RM0","OI0","M0","NI0",'AG0','RB0'}
+    symbol_set={"RM0","OI0","M0","NI0",'AG0','RB0','AU0','JD0','RB0','RU0',
+                'TL0','TS0','T0','TF0','JD0'}
     # minite
     for symbol in symbol_set:
         try:
